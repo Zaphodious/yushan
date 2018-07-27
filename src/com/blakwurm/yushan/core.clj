ccfef11a61c519ab0134bdf689ce6dfd95283d95
Alex Chythlook <achythlook@gmail.com>
2018-07-27 12:59:29 -0800

    Put in filtering by queries (on the generated data set)

--- src/com/blakwurm/yushan/core.clj
+++ src/com/blakwurm/yushan/core.clj
@@ -6,10 +6,38 @@
             [clojure.spec.gen.alpha :as gen]
             [com.blakwurm.lytek.spec]))
 
+(defn optional [a] (schema/optional-key a))
+(defn required [a] a)
+
+(def query-params
+  {:character-type {:opt? optional :pristmatic-type String :param-type :query-filter}
+   :name           {:opt? optional :pristmatic-type String :param-type :query-filter}
+   :id             {:opt? optional :pristmatic-type String :param-type :query-filter}
+   :category       {:opt? optional :pristmatic-type String :param-type :query-filter}
+   :amount         {:opt? optional :pristmatic-type Number :param-type :query-modifier}
+   :owner          {:opt? optional :pristmatic-type String :param-type :query-filter}})
+
+(defn params-to-prismatic [qp's]
+  (into {}
+        (map (fn [[query-key {:keys [opt? pristmatic-type param-type]}]]
+               [(opt? query-key) pristmatic-type])
+             qp's)))
+
+(defn params-by-param-type [qp's]
+  (reduce (fn [ds [query-key {:keys [opt? pristmatic-type param-type]}]]
+            (assoc ds param-type (conj (or (get ds param-type) []) query-key)))
+          {}
+          qp's))
+
 (defonce *db (atom {}))
 
 (defn filter-data [params]
-  (let []))
+  (let [{:keys [query-filter]} (params-by-param-type query-params)
+        target-map (select-keys params query-filter)
+        subfilter-keys (keys target-map)]
+    (filter (fn [a]
+              (= target-map (select-keys a subfilter-keys)))
+            (vals @*db))))
 
 (defn reset-db []
   (reset! *db (->> (gen/sample (s/gen :lytek/solar))
@@ -21,11 +49,11 @@
   (let [{:keys [character-type id owner category] :as params}
         (:query (:parameters request))]
     (println "request is " request)
-    {:resp 0
+    {:resp   0
      :params params
-     :data (vals @*db)
+     :data   (filter-data params)
      ;(str (:query (:parameters request)))
-     :error ""}))
+     :error  ""}))
 
 (defn api-create [request]
   {:resp 0 :data [] :error ""})
@@ -36,33 +64,24 @@
 (defn api-delete [request]
   {:resp 0 :data [] :error ""})
 
-(defn optional [a] (schema/optional-key a))
-(defn required [a] a)
-
-(def query-params
-  {(optional :character-type) String
-   (optional :id) String
-   (optional :owner) String
-   (optional :results) Number
-   (optional :category) String})
 
 (def api-v1-resource
   (yada/resource
-    {:produces "application/json"
-     :methods {:get #'api-read
-               :put #'api-update
-               :post #'api-create
-               :delete #'api-delete}
-     :parameters {:query query-params}}))
+    {:produces   "application/json"
+     :methods    {:get    #'api-read
+                  :put    #'api-update
+                  :post   #'api-create
+                  :delete #'api-delete}
+     :parameters {:query (params-to-prismatic query-params)}}))
 
 (defonce *server
          (atom {}))
 
 (defn start-server []
   (reset! *server
-    (yada/listener
-      ["/api/" {"asdf/v1/" api-v1-resource}]
-      {:port 3000})))
+          (yada/listener
+            ["/api/" {"asdf/v1/" api-v1-resource}]
+            {:port 3000})))
 
 (defn stop-server []
   ((:close @*server)))
