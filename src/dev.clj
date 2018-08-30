3b9908e29957ace5829818f4a6b49e3723a58dfd
Alex Chythlook <achythlook@gmail.com>
2018-08-29 20:47:59 -0800

    Add in some good, old-fashioned cider-nrepl

--- src/dev.clj
+++ src/dev.clj
@@ -1,17 +1,23 @@
 (ns dev
   (:require [clojure.tools.nrepl.server :as nrepl]
             [com.blakwurm.yushan.core :as yushan]
-            [clojure.tools.namespace.repl :as namespace.repl]))
+            [clojure.tools.namespace.repl :as namespace.repl]
 
 (println "user ns loaded")
 
 (defn foo []
   (println "booyeah!"))
 
-(def *nrepl-server (atom {}))
+(def *nrepl-server (atom nil))
+
+(defn cider-handler []
+  (require 'cider.nrepl)
+  (ns-resolve 'cider.nrepl 'cider-nrepl-handler))
 
 (defn start-nrepl []
-  (reset! *nrepl-server (nrepl/start-server :port 4242)))
+  (when-not @*nrepl-server
+    (reset! *nrepl-server (nrepl/start-server :port 4242 :handler (cider-handler))))
+  (println "Ripple In!"))
 
 (defn stop-nrepl []
   (nrepl/stop-server @*nrepl-server))
@@ -26,15 +32,14 @@
   (do
     (start-server)))
 
+(defn start-cider []
+  (nrepl-server/start-server :port 4242 :handler (cider-handler))) 
+
 (defn reset []
   (do
     (stop-server)
     (namespace.repl/refresh)
     (com.blakwurm.yushan.core/start-server)))
-
-(defn start-nrepl []
-  (nrepl/start-server :port 4242)
-  (println "Ripple In!"))
 
 (defn -main []
   (start-nrepl))
