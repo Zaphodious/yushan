0b6f9e75e9ee4a10283d11ad05d7e1082c57e680
Alex Chythlook <achythlook@gmail.com>
2018-09-02 00:10:20 -0800

    Make the server not require restarts when resources change

--- src/com/blakwurm/yushan/core.clj
+++ src/com/blakwurm/yushan/core.clj
@@ -4,6 +4,10 @@
             [bidi.bidi :as bidi]
             [org.httpkit.server :as httpkit-server :only [run-server]]
             [bidi.ring]
+            [ring.middleware.keyword-params :as middleware.keyword-params]
+            [ring.middleware.params :as middleware.params]
+            [ring.middleware.content-type :as middleware.content-type]
+            [ring.middleware.not-modified :as middleware.not-modified]
             [schema.core :as schema]
             [clojure.test.check.generators]
             [ring.util.response :as res]
@@ -18,14 +22,32 @@
             [spec-coerce.core :as sc]
             [clojure.tools.namespace.repl :as namespace.repl]))
 
+(defn give-a-thing [request]
+  (pr-str {:request request
+           :testthing "Turns out you need to connect anew each time"}))
+
+;; In order to dev quickly, we abuse clojure's var system.
+;; The following weirdness is written this way so that
+;; we don't have to restart the server after every change. 
 (defn simple-handler [a]
-    (res/response "Respondo-man"))
+    (res/response (#'give-a-thing a)))
 
 (def routes
-  ["/" #'simple-handler])
+  ["/" {"" #'simple-handler
+        "thing" #'simple-handler
+        "api" {"/entities" {"/v1" #'simple-handler}}}])
 
 (def route-handler
   (bidi.ring/make-handler routes))
+
+(defn make-middleware []
+ (-> #'route-handler                
+     middleware.keyword-params/wrap-keyword-params
+     middleware.params/wrap-params
+     middleware.content-type/wrap-content-type
+     middleware.not-modified/wrap-not-modified))
+
+(def middlewares (make-middleware))
 
 (def *server (atom nil))
 
@@ -38,7 +60,7 @@
   ;; (db/use-database! "jdbc:mysql://localhost/test" "user" "password")
 
   ;; other init staff, like init-db, init-redis, ...
-  (reset! *server (httpkit-server/run-server #'route-handler) {:port 3000}))
+  (reset! *server (httpkit-server/run-server #'middlewares {:port 3000})))
 
 (defn stop-server []
   (when-not (nil? @*server)
@@ -49,6 +71,4 @@
 
 (defn restart-server []
   (stop-server)
-  (Thread/sleep 1000)
   (start-server))
-
