6dc6cbd2dda234be7e294e1c9eaf5e72f2a1f653
Alex Chythlook <achythlook@gmail.com>
2018-09-03 15:28:29 -0800

    Begin adding actual api object logic

--- src/com/blakwurm/yushan/api_object.clj
+++ src/com/blakwurm/yushan/api_object.clj
@@ -1,7 +1,40 @@
 (ns com.blakwurm.yushan.api-object
     (:requre [liberator.core :as liberator :refer [resource defresource]]))
 
+(def sample-api-def
+  {:name :examplar
+   :columns {:name [:string]
+             :id [:string :primary :key]
+             :description [:string]
+             :rest [:string]}
+   :dessicate (fn [a] a)
+   :hydrate (fn [a] a)}) 
+
+(def sample-api-def-doc
+  {:name "A keyword denoting the resource's name."
+   :column "A map of keyword column names to their sql column spec. Passed into yushan.db/make-table as :column-info."
+   :dessicate "Lambda of thing to stored-thing. Transforms the thing for storage in the database."
+   :hydrate "Lambda of stored-thing to thing. Transforms the thing from what's stored in the database to a usable thing."})
+
+(defmulti api-object-for identity)
+
+(def template-api-shell
+  {:resp 0
+   :error ""
+   :data []})
+
+(def resource-defaults
+  {:available-media-types ["application/json"]})
+
+(defn liberator-resource-for [name-key]
+   (let [api-def-of (api-def name-key)]
+      {:available-media-types ["application/json"]}))
+       
+
 (defn make-api-object [{:keys [] :as param-map}]
   (resource
     :available-media-types ["application/json"]
     :handle-ok {:thing "badboi"}))
+
+
+
